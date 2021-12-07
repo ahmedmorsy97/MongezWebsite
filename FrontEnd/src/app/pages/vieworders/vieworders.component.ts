516062ca0a1d89949f2117b247a610400d46507d
omarrifky <omarkhaledrifky@hotmail.com>
2021-12-07 22:12:38 +0200

    vieworders

--- FrontEnd/src/app/pages/vieworders/vieworders.component.ts
+++ FrontEnd/src/app/pages/vieworders/vieworders.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { faSearch, faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faCartPlus,faMinus,faPlus } from '@fortawesome/free-solid-svg-icons';
+import { IMultiSelectSettings, IMultiSelectTexts } from 'ngx-bootstrap-multiselect';
 
 @Component({
   selector: 'app-vieworders',
@@ -8,11 +9,54 @@ import { faSearch, faCartPlus } from '@fortawesome/free-solid-svg-icons';
 })
 export class ViewordersComponent implements OnInit {
   faSearch = faSearch;
+  suppliers = [];
+  categories=[];
+  filters={purchasedate:false,supplier:false,category:false,status:false};
+  faPlus = faPlus;
+  faMinus = faMinus;
+  // Settings configuration
+mySettings: IMultiSelectSettings = {
+  enableSearch: true,
+  showCheckAll:true,
+  showUncheckAll:true,
+  maintainSelectionOrderInTitle:true,
+  checkedStyle: 'fontawesome',
+  buttonClasses: 'btn btn-primary',
+  dynamicTitleMaxItems: 3,
+  displayAllSelectedText: true
+};
+
+// Text configuration
+myTexts: IMultiSelectTexts= {
+  checkAll: 'Select all',
+  uncheckAll: 'Unselect all',
+  checked: 'item selected',
+  checkedPlural: 'items selected',
+  searchPlaceholder: 'Find',
+  searchEmptyResult: 'Nothing found...',
+  searchNoRenderText: 'Type in search box to see results...',
+  defaultTitle: 'Select',
+  allSelected: 'All selected',
+};
   constructor() { }
 
   ngOnInit(): void {
+    this.suppliers = [
+      { id: 1, name: 'Supplier 1' },
+      { id: 2, name: 'Supplier 2' },
+      { id: 3, name: 'Supplier 3' },
+      { id: 4, name: 'Supplier 4' },
+  ];
+  this.categories = [
+    { id: 1, name: 'Mechanical' },
+    { id: 2, name: 'Electrical' },
+    { id: 3, name: 'Hardware' },
+];
   }
   ViewOrder(){
 
   }
+  togglefilter(type){
+    this.filters[type] = !this.filters[type]
+      }
 }
