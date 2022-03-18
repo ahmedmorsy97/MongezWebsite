83a9e6bb7eb3959293c4a608f2ce78be17ccfbb8
Ahmed Samir Morsy <ahmed.samir@gameball.co>
2022-03-18 16:23:10 +0200

    enhancements

--- BackEnd/config/index.js
+++ BackEnd/config/index.js
@@ -1,6 +1,4 @@
-const secretOrPrivateKey =
+export const secretOrPrivateKey =
   `${process.env.NODE_ENV}` === "production"
     ? process.env.JWT_SECRET
     : "secretOrPrivateKey";
-
-    module.exports = { secretOrPrivateKey };
